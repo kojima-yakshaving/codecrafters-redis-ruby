adb86870915cf62405e2428219ba6d7730daf87b
malkoG <rijgndqw012@gmail.com>
2024-05-13 03:17:15 +0900

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -5,22 +5,64 @@ class YourRedisServer
     @port = port
   end
 
-  def handle(input)
-    puts input
-  end
-
   def start
     server = TCPServer.new(@port)
     loop do
-      # Thread.new(server.accept) do |client|
-        client = server.accept
-        rbuf = client.read
-        translated = handle(rbuf)
-        
+      Thread.new(server.accept) do |session|
+        connection = RedisConnection.new(session)
+        until connection.eof?
+          connection.execute_command
+        end
 
-        client.close
-      # end
+        connection.close
+      end
     end
+  end
+end
+
+
+class RedisConnection
+  def initialize(session)
+    @session = session
+  end
+
+  def execute_command
+    command, *params = handle_input
+    if command == "ECHO"
+      body = params.join(' ')
+      writeline("$#{body.size}\r\n#{body}\r\n")
+    elsif command == "PING"
+      writeline("+PONG\r\n")
+    end
+  end
+
+  def readline
+    @session.gets.chomp
+  end
+
+  def eof?
+    @session.eof?
+  end
+
+  def writeline(...)
+    @session.puts(...)
+  end
+
+  def handle_input
+    _, *args = readline.chars
+    args_count = args.join.to_i
+
+    argv = []
+    args_count.times do 
+      _ = readline
+      argv << readline
+    end
+
+    argv
+  end
+
+  def close 
+    @session.close
   end
 end
 
